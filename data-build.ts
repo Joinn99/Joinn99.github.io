aed6e13ca98e3d46bc3bad73dfee0fc183b6cb3c
Joinn99 <Joinnnor@outlook.com>
2025-11-23 12:17:33 +0800

    Fix data load

---
+++ data-build.ts
@@ -0,0 +1,3 @@
+// This file is a placeholder and will be overwritten by the build script.
+// It's used to prevent TypeScript errors in your editor before the first build.
+export const prebuiltData = {};
